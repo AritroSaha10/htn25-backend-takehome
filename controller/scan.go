eb8a37e1b87c088f2c1760e3e1f65b9a9142533c
Aritro Saha <29025984+AritroSaha10@users.noreply.github.com>
2025-02-09 11:42:03 -0500

    feat: add PUT request for scanning user

--- controller/scan.go
+++ controller/scan.go
@@ -2,18 +2,57 @@ package controller
 
 import (
 	"net/http"
+	"strconv"
 
+	"github.com/AritroSaha10/htn25-backend-takehome/lib"
+	"github.com/AritroSaha10/htn25-backend-takehome/model"
+	"github.com/AritroSaha10/htn25-backend-takehome/util"
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/render"
 )
 
 type ScanController struct{}
 
 func (c ScanController) Routes() chi.Router {
 	r := chi.NewRouter()
-	r.Get("/", c.GetAllScans)
+	r.Put("/{id}", c.ScanUser)
 	return r
 }
 
-func (c ScanController) GetAllScans(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello, Scans!"))
+func (c ScanController) ScanUser(w http.ResponseWriter, r *http.Request) {
+	// Get the user ID from the URL parameter
+	id := chi.URLParam(r, "id")
+	idUint64, err := strconv.ParseUint(id, 10, 64)
+	if err != nil {
+		render.Render(w, r, util.ErrBadRequestRender("id is not unsigned int", err))
+		return
+	}
+	idUint := uint(idUint64)
+
+	// Check if the user exists
+	if lib.GetDB().Where("id = ?", idUint).Limit(1).Find(&model.User{}).RowsAffected == 0 {
+		render.Render(w, r, util.ErrBadRequestRender("user does not exist", nil))
+		return
+	}
+
+	// Parse request body into a Scan struct
+	scan := model.Scan{
+		UserID: idUint,
+	}
+	if err := render.Bind(r, &scan); err != nil {
+		render.Render(w, r, util.ErrBadRequestRender("invalid request body", err))
+		return
+	}
+
+	// Add scan to database
+	err = model.CreateScan(lib.GetDB(), &scan)
+	if err != nil {
+		render.Render(w, r, util.ErrServerRender(err))
+		return
+	}
+
+	if err := render.Render(w, r, &scan); err != nil {
+		render.Render(w, r, util.ErrRender(err))
+		return
+	}
 }
--- model/scan.go
+++ model/scan.go
@@ -1,6 +1,8 @@
 package model
 
 import (
+	"fmt"
+	"net/http"
 	"time"
 
 	"gorm.io/gorm"
@@ -17,4 +19,26 @@ type Scan struct {
 	ActivityCategory string         `json:"activity_category" gorm:"not null"`
 	ScannedAt        time.Time      `json:"scanned_at" gorm:"not null"`
 	UserID           uint           `json:"-" gorm:"not null"`
+}
+
+// Bind implements the render.Binder interface for Scan.
+func (s *Scan) Bind(r *http.Request) error {
+	if s.ActivityName == "" || s.ActivityCategory == "" {
+		return fmt.Errorf("activity_name and activity_category are required")
+	}
+
+	return nil
+}
+
+// Render implements the render.Renderer interface for Scan.
+func (s *Scan) Render(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+func CreateScan(db *gorm.DB, scan *Scan) error {
+	scan.ScannedAt = time.Now()
+	if err := db.Create(&scan).Error; err != nil {
+		return err
+	}
+	return nil
 }
